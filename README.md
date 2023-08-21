b390ac0c7479a2551263cdfccf87b2a6a828c57d
Simon Stipcich <95277988+stiproot@users.noreply.github.com>
2023-08-21 18:52:42 +0000

    feat:better README.

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # asq
+[This README is very much a work in progress.]
+
+## About
+ASQ is platform that allows for the scheduling of meetings, publishing of blog posts, and integrates with Zoom.
+
+The idea is centered around property developers and home renovators being able to access property centric information and services. 
--- src/db/mysql/test/test-data-notification.mysql.sql
+++ src/db/mysql/test/test-data-notification.mysql.sql
@@ -15,6 +15,6 @@ values
     'http://localhost:3000/image/2e5a8ade-bac6-4ccf-b9f5-538da2f84fa4/ec2b1da8-6e5b-44ee-9ad6-4951fa547a7d.jpeg',
     '',
     'htttp://localhost:4200/meeting/3837f0f9-30cc-4266-bd56-568f9dcf0fad',
-    'https://zoom.us/s/94543626416?zak=eyJ6bV9za20iOiJ6bV9vMm0iLCJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhdWQiOiJjbGllbnQiLCJ1aWQiOiJTMHN6dHN4TFJYR2diTjhmTTNyYU1nIiwiaXNzIjoid2ViIiwic3R5Ijo5OSwid2NkIjoiYXcxIiwiY2x0IjowLCJzdGsiOiI3QXN6UkZRb0hKNGtjSG9fQjJRZm5RemhVcnVEVUEtdU9kLUdZZ0dPa0o4LkJnWWdPRGM0V1ZVNFp6Uk1TRm94WkcweGJrVnlTMWRxZWpJNE9Xd3ZiMkZyV2pBQUFBd3pRMEpCZFc5cFdWTXpjejBBQTJGM01RQUFBWGhhcVA5RkFCSjFBQUFBIiwiZXhwIjoxNjI0MjA0NzI4LCJpYXQiOjE2MTY0Mjg3MjgsImFpZCI6IkI4RlN5WGVUUmJhSkhjbEVSQldCZHciLCJjaWQiOiIifQ.ZRE8IRSFZvq3EN6IYQGuVBhtzFid7PrQzHguxC_HwEo',
+    'https://zoom.us/s/94543626?zak=eyJ6bV9za20iOiJ6bV9vMm0iL.uijkkjjkjkkj.ZRE8IRSFZvq3EN6IYQGuVBhtzFid7PrQzHguxC_HwEo',
     0
 )
